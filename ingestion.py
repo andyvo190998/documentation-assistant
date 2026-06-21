65341219fb022c3f9a05b6e62598e48be0d3a296
Andy Vo <hoangbao.vo@jsmd-group.com>
2026-06-21 15:25:47 +0200

    crawl data with tavily

---
+++ ingestion.py
@@ -0,0 +1,53 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+from dotenv import load_dotenv
+
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_warning, log_info, log_success)
+
+load_dotenv()
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small", show_progress_bar=False, chunk_size=50, retry_min_seconds=10
+)
+
+vectorstore = PineconeVectorStore(embedding=embeddings, index_name=os.environ.get("INDEX_NAME"))
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
+async def main():
+    """Main async function to orchestrate the entire process."""
+
+    log_info(
+        "TavilyCrawl: Starting to crawl documentation from https://python.langchain.com/",
+        Colors.PURPLE,
+    )
+
+    res = tavily_crawl.invoke({
+        "url": "https://python.langchain.com/",
+        "max_depth": 1,
+        "extract_depth": "advanced",
+        "instruction": "content on ai agents"
+    })
+    all_docs = [Document(page_content=result['raw_content'], metadata={"source": result['url']})
+                for result in res['results']]
+    log_success(
+        f"TavilyCrawl: Successfully crawled {len(all_docs)} documents.",
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
